49a241cfb348da1ca94b57cc01955ea67fc080cd
tomej-dev <joaocarlos8221@gmail.com>
2025-10-24 01:03:56 -0300

    Adicionar arquivos de projeto.

---
+++ StaticMembers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using StaticMembers.utils;
+
+namespace StaticMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Product p = new Product("TV", 500.00, 10);
+
+                Console.WriteLine("Dados do produto: " + p.GetNome() + ", $" + p.GetPreco() + ", " + p.GetQuantidade() + " unidades, Total: $" + p.ValorTotalEmEstoque());
+                Console.WriteLine();
+                Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+
+                int qte = int.Parse(Console.ReadLine());
+                p.AdicionarProdutos(qte);
+
+                Console.WriteLine();
+                Console.WriteLine("Dados atualizados: " + p.GetNome() + ", $" + p.GetPreco() + ", " + p.GetQuantidade() + " unidades, Total: $" + p.ValorTotalEmEstoque());
+                Console.WriteLine();
+                Console.Write("Digite o número de produtos a ser removido do estoque: ");
+
+                qte = int.Parse(Console.ReadLine());
+                p.RemoverProdutos(qte);
+
+                Console.WriteLine();
+                Console.WriteLine("Dados atualizados: " + p.GetNome() + ", $" + p.GetPreco() + ", " + p.GetQuantidade() + " unidades, Total: $" + p.ValorTotalEmEstoque()); 
+        }
+    }
+}
---
+++ StaticMembers/utils/Product.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMembers.utils
+{
+    internal class Product
+    {
+        private string Nome;
+        private double Preco;
+        private int Quantidade;
+
+        public Product(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public double GetPreco()
+        {
+            return Preco;
+        }
+
+        public void SetPreco(double preco)
+        {
+            this.Preco = preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return Quantidade;
+        }
+
+        public void SetQuantidade(int quantidade)
+        {
+            this.Quantidade = quantidade;
+        }
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public string DadosDoProduto()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2")
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2");
+        }
+    }
+}
